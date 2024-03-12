64049a2ff59c27b6d35132a058d2a78dabb75af6
FELIPE ROBERTO CASSIANO <felipe.rcassiano@senacsp.edu.br>
2024-03-12 11:31:58 -0300

    Site Hollow Knight usando JS

---
+++ HKnight/script.js
@@ -0,0 +1,15 @@
+let modal = document.getElementById("meuModal");
+let btn = document.getElementsByClassName("btn");
+let fechado = document.getElementsByClassName("fechado")[0]
+
+
+function aberto() {
+    modal.style.display="block"
+}
+
+
+function fechar() {
+    modal.style.display="none"
+}
+
+let nomedeAlunos = ['Lucas','Felipe','Paulo','Willy'];
